d6f61840a2cb63d31add2d827b74cc8d85007476
Yash2003Bisht <yash2003bisht>
2022-06-26 20:59:53 +0530

    adding a function to get data into csv file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 import requests
+import pandas as pd
 
 def print_data(data: dict):
     for key, value in data.items():
         print(f"{key}: {value}")
+
+def get_data_as_csv(json_data):
+    data = pd.DataFrame(
+        {
+            "Valid": [json_data["valid"]],
+            "Number": [json_data["number"]],
+            "Local Format": [json_data["local_format"]],
+            "International Formate": [json_data["international_format"]],
+            "Country Prefix": [json_data["country_prefix"]],
+            "Country Code": [json_data["country_code"]],
+            "Country Name": [json_data["country_name"]],
+            "Location": [json_data["location"]],
+            "Carrier": [json_data["carrier"]],
+            "Line Type": [json_data["line_type"]],
+        }
+    )
+    data.to_csv("data.csv")
+    print("data saved in data.csv file.")
 
 def main():
     url = "https://api.apilayer.com/number_verification/validate?number={}"
@@ -13,7 +32,9 @@ def main():
     response = requests.request("GET", url.format(number), headers=headers)
 
     if response.ok:
-        print_data(response.json())
+        data = response.json()
+        print_data(data)
+        get_data_as_csv(data)
     else:
         print("Error! please check if you add your api or not?")
 
